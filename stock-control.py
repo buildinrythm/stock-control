f70762ae20f1bfac4b555567807d417d782e133e
Joshua Agbor <123345261+Zaunsrevenge@users.noreply.github.com>
2026-01-28 14:10:15 +0000

    Added welcome message and choice for good in or out

--- stock-control.py
+++ stock-control.py
@@ -6,6 +6,12 @@ class Goods:
         self.prodCode = prodCode
         self.quantity = quantity
 
+    def welcomeMessage(): 
+        print("=" * 50)
+        print("Welcome to the stock control system!")
+        print("=" * 50)
+
+
 class GoodsIn(Goods): 
 
     chargePerProduct = 5.00
@@ -20,3 +26,24 @@ class GoodsOut(Goods):
     def __init__(self, prodCode, quantity):
         super().__init__(prodCode, quantity)
 
+    def getReg(self):
+        self.getReg = input("What is your car's registration number?" "\n")
+
+def main():
+    # Welcome User
+    Goods.welcomeMessage()
+    choice = input("Enter 1 for (Goods in) Enter 2 for (Goods out)" '\n')
+
+    # Get user choice
+    if choice == 1:
+        print("Goods in confirmed")
+    else: 
+        print("Goods out confirmed")
+    
+
+
+
+
+# Run the program
+if __name__ == "__main__":
+    main()
